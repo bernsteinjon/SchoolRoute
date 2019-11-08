3bed7bc4f4660e84ffca9d652b33251e44f4a011
System Administrator <sysadmin@hpc308.tcnj.edu>
2019-11-08 17:15:32 -0500

    implemented login

--- src/SchoolRoute/app/controllers/application_controller.rb
+++ src/SchoolRoute/app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
 
+  helper_method :current_user
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+  end
+
 	def index
 		@posts = Post.all
 	end
--- src/SchoolRoute/config/routes.rb
+++ src/SchoolRoute/config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#login'
   get 'home/parent'
   get 'home/login'
   get 'posts/index'
   get 'posts/new'
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   get 'about' => 'pages#about' 
 
