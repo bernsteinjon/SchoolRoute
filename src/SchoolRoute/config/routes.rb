c5bdb5ca9a2196ada8fddfe8c4de0324b307a06b
Gavin Bowman <bowmang1@tcnj.edu>
2019-11-09 18:58:31 -0500

    tidied up login system and permissions

--- src/SchoolRoute/config/routes.rb
+++ src/SchoolRoute/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'sessions/destroy'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #root 'sessiosn#new'
   root 'home#login'
   get 'home/parent'
   get 'home/login'
